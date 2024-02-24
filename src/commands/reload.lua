0ff709fae266e0d9e1eb06405e0b3feab6476645
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2024-02-24 12:55:05 +0200

    Added the new config reload method with Swiftly Admins 🧐

--- src/commands/reload.lua
+++ src/commands/reload.lua
@@ -1,5 +1,7 @@
 function ReloadConfigCommand()
     commands:Register("discord_reload", function(playerid, args, argsCount, silent)
+        local IsAdmin = exports["swiftly_admins"]:CallExport("HasFlags", playerid, "z")
+    
         if playerid == -1 then
             config:Reload("discord")
             print("The config has been reloaded.")
@@ -7,8 +9,14 @@ function ReloadConfigCommand()
             local player = GetPlayer(playerid)
             if not player then return end
     
-            config:Reload("discord")
-            player:SendMsg(MessageType.Chat, config:Fetch("discord.ChatCommandsMessages.ReloadConfig.ReloadCommandMessage"))
+            if IsAdmin == 1 then
+                config:Reload("discord")
+                player:SendMsg(MessageType.Chat, config:Fetch("discord.ChatCommandsMessages.ReloadConfig.ReloadConfigMessage"))
+            end
+    
+            if IsAdmin == 0 then
+                player:SendMsg(MessageType.Chat, config:Fetch("discord.ChatCommandsMessages.NoPremissions.NoPremissionsMessage"))
+            end
         end
     end)
 end
